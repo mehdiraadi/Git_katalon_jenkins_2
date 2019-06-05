b052020ecdd0e5670fe23df602e3a9035f9a2c4a
Jasmi Shaheer <jasmi@doctrin.se>
2019-06-05 14:00:50 +0200

    Added project with 1 tc

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e0b83eb8-a798-4ee7-ac69-03dc2f79736b</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb22432c-ba75-40a6-9599-878d3594828f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
